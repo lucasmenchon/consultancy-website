fc5513fffd4f3c66f9ff78c185c5dd66007a5aed
lucasmenchon <developer@luccas.dev>
2023-12-12 21:01:55 -0300

    add favicon, updates

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -1,33 +1,31 @@
 ﻿<template>
-    <nav class="navbar navbar-expand-lg" :style="{ backgroundColor: navBackgroundColor }">
+    <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
-            <a class="navbar-brand">
-                <img class="logo" src="../assets/logo.png">            
+            <a class="navbar-brand me-auto">
+                <img class="logo" src="../assets/logo.png">
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item me-4" v-for="route in routes" :key="route.path">
-                        <router-link class="nav-link" :to="route.path" :class="{ 'active': isActive(route) }">{{ capitalizeFirstLetter(route.name) }}</router-link>
+                        <router-link class="nav-link" :to="route.path" :class="{ 'active': isActive(route) }">{{
+                            capitalizeFirstLetter(route.name) }}</router-link>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
 </template>
+
 <script>
 export default {
     data() {
         return {
             routes: this.$router.getRoutes(),
             activeRoute: ''
-        }
-    },
-    computed: {
-        navBackgroundColor() {
-            return '#013878'; // Cor de fundo da barra de navegação
         }
     },
     methods: {
@@ -48,17 +46,23 @@ export default {
     }
 }
 </script>
+
 <style scoped>
-    .navbar a{
-        color: #fff;
-    }
+.navbar {
+    background-color: transparent;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
 
-    .navbar .nav-link.active {
-        color: #EE1B2E;
-    }
+.navbar a {
+    color: #000;
+}
 
-    .logo{
-        height: 50px;
-        width: auto;
-    }
+.navbar .nav-link.active {
+    color: #EE1B2E;
+}
+
+.logo {
+    height: 60px;
+    width: auto;
+}
 </style>
